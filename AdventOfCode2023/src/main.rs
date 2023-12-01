af3924edff6e43cd1af33f803989b78a30a38eee
Anthony Dunford <trashzy21@gmail.com>
2023-12-01 07:28:35 +0000

    day1 first attempt

---
+++ AdventOfCode2023/src/main.rs
@@ -0,0 +1,84 @@
+use core::num;
+use std::{fs, collections::HashMap};
+
+fn main() {
+
+    let example_input = 
+    "1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet";
+
+    let input_path = "inputs/day1/input.txt";
+        
+    let input = fs::read_to_string(input_path)
+        .expect("Should have been able to read the file");
+
+    part1(example_input);
+    part1(&input);
+
+    let example_input2 = "two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen";
+
+    let answer = part2(example_input2);
+
+    println!("{}", answer);
+
+}
+
+fn part1(input: &str) -> i32 {
+    let parts = input.split("\n");
+
+    let sum = parts.fold(0,  |sum, part| {
+
+        let int_itr = part.chars().filter_map(|c| {
+            c.to_string().parse::<i32>().ok()
+        });
+
+        let int_vec = int_itr.collect::<Vec<i32>>();
+        return sum + int_vec.first().unwrap() * 10 + int_vec.last().unwrap();
+    });
+
+    return sum
+}
+
+fn part2(input: &str) -> i32 {
+    let number_names = HashMap::from([
+        ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), 
+        ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)
+    ]);
+    let parts = input.split("\n");
+
+    //let mut r = "";
+    /// RUST IS STUPID
+
+    // number_names.keys().fold(part, |s, &key| {
+    //     let v = number_names[&key].to_string();
+    //     r = s.replace(key, &v).as_str();
+    //     return r;
+    // });
+
+    let sum = parts.fold(0,  |sum, part| {
+
+        let x = part.replace("one", "1").replace("two", "2").replace("three", "3")
+        .replace("four", "4").replace("five", "5").replace("six", "6")
+        .replace("seven", "7").replace("eight", "8").replace("nine", "9");
+        
+        println!("{}", x);
+
+        let int_itr = x.chars().filter_map(|c| {
+            c.to_string().parse::<i32>().ok()
+        });
+
+        let int_vec = int_itr.collect::<Vec<i32>>();
+        println!("{}", int_vec.first().unwrap() * 10 + int_vec.last().unwrap());
+        return sum + int_vec.first().unwrap() * 10 + int_vec.last().unwrap();
+    });
+
+    return sum;
+}
