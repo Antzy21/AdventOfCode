51dfa8c8cd688838c4c29230659de3094073fb18
Anthony Dunford <Anthony@Dunford.uk.com>
2024-12-20 13:46:43 +0000

    Day 20 Part 2

--- AdventOfCode2024/Day20.cs
+++ AdventOfCode2024/Day20.cs
@@ -10,43 +10,76 @@ public class Day20 : IDaySolution
     {
         _map = ParseInput();
 
+        int maxCheatLength = 2;
+
         var startPos = FindCharOnMap(_map, 'S');
-        PosAndCheatPos startPosWithoutCheat = new(startPos, null);
         var endPos = FindCharOnMap(_map, 'E');
-        PosAndCheatPos endPosWithoutCheat = new(endPos, null);
 
-        Dictionary<PosAndCheatPos, int> exploredPositions = [];
-        exploredPositions.Add(startPosWithoutCheat, 0);
+        Dictionary<Position, int> exploredPositions = [];
+        exploredPositions.Add(startPos, 0);
 
-        var currentStatesToExplore = new List<PosAndCheatPos>() { startPosWithoutCheat };
+        var currentStatesToExplore = new List<Position>() { startPos };
 
         while (currentStatesToExplore.Count > 0)
         {
             currentStatesToExplore = ExploreMap(exploredPositions, currentStatesToExplore);
         }
 
-        var endStates = exploredPositions.Where(kvp => kvp.Key.Pos == endPos);
+        var endStates = exploredPositions.Where(kvp => kvp.Key == endPos);
 
-        Console.WriteLine($"{exploredPositions[endPosWithoutCheat]}");
-        Console.WriteLine($"---");
+        var result = FindCheatTimeSavers(maxCheatLength, exploredPositions);
 
-        var v = endStates
-            .Where(es => es.Value < exploredPositions[endPosWithoutCheat])
-            .Where(es => 100 < exploredPositions[endPosWithoutCheat] - es.Value)
-            .GroupBy(es => es.Value)
-            .Select(ges => $"{ges.Count()}:{exploredPositions[endPosWithoutCheat] - ges.Key}");
-
-        Console.WriteLine($"{string.Join(", ", v)}");
-
-        return endStates
-            .Where(es => es.Value < exploredPositions[endPosWithoutCheat])
-            .Where(es => 100 <= exploredPositions[endPosWithoutCheat] - es.Value)
-            .Count();
+        return result;
     }
 
     public int? SolvePart2()
     {
-        return 0;
+        _map = ParseInput();
+
+        int maxCheatLength = 20;
+
+        var startPos = FindCharOnMap(_map, 'S');
+        var endPos = FindCharOnMap(_map, 'E');
+
+        Dictionary<Position, int> exploredPositions = [];
+        exploredPositions.Add(startPos, 0);
+
+        var currentStatesToExplore = new List<Position>() { startPos };
+
+        while (currentStatesToExplore.Count > 0)
+        {
+            currentStatesToExplore = ExploreMap(exploredPositions, currentStatesToExplore);
+        }
+
+        var endStates = exploredPositions.Where(kvp => kvp.Key == endPos);
+
+        var result = FindCheatTimeSavers(maxCheatLength, exploredPositions);
+
+        return result;
+    }
+
+    private static int FindCheatTimeSavers(int maxCheatLength, Dictionary<Position, int> exploredPositions, int minSavings = 100)
+    {
+        var cheatSavesLessThan100 = 0;
+
+        foreach (var kvp in exploredPositions)
+        {
+            for (int i = maxCheatLength * -1; i <= maxCheatLength; i++)
+            {
+                var jLimit = maxCheatLength - Math.Abs(i);
+                for (int j = jLimit * -1; j <= jLimit; j++)
+                {
+                    var newPos = new Position(kvp.Key.X - i, kvp.Key.Y - j);
+                    if (exploredPositions.TryGetValue(newPos, out var newPosScore))
+                    {
+                        if (newPosScore - exploredPositions[kvp.Key] >= minSavings + Math.Abs(i) + Math.Abs(j))
+                            cheatSavesLessThan100++;
+                    }
+                }
+            }
+        }
+
+        return cheatSavesLessThan100;
     }
 
     private static Position FindCharOnMap(char[][] map, char c)
@@ -62,21 +95,20 @@ public class Day20 : IDaySolution
         throw new Exception("Can't find start");
     }
 
-    private List<PosAndCheatPos> ExploreMap(Dictionary<PosAndCheatPos, int> exploredPositions, List<PosAndCheatPos> currentStatesToExplore)
+    private List<Position> ExploreMap(Dictionary<Position, int> exploredPositions, List<Position> currentStatesToExplore)
     {
-        var newCurrentStates = new List<PosAndCheatPos>();
+        var newCurrentStates = new List<Position>();
 
         foreach (var posAndCheat in currentStatesToExplore)
         {
             var newScore = exploredPositions[posAndCheat] + 1;
-            IEnumerable<PosAndCheatPos> newPositions = GetNewPositions(posAndCheat);
 
-            foreach (var newPosAndCheat in newPositions)
+            foreach (var newPos in GetNewPositions(posAndCheat))
             {
-                if (!exploredPositions.TryGetValue(newPosAndCheat, out var existingScore) || newScore < existingScore)
+                if (!exploredPositions.TryGetValue(newPos, out var existingScore) || newScore < existingScore)
                 {
-                    exploredPositions[newPosAndCheat] = newScore;
-                    newCurrentStates.Add(newPosAndCheat);
+                    exploredPositions[newPos] = newScore;
+                    newCurrentStates.Add(newPos);
                 }
             }
         }
@@ -85,27 +117,16 @@ public class Day20 : IDaySolution
         return currentStatesToExplore;
     }
 
-    private IEnumerable<PosAndCheatPos> GetNewPositions(PosAndCheatPos posAndCheat)
+    private IEnumerable<Position> GetNewPositions(Position pos)
     {
-        var lst = new List<Position>() {
-                    new(posAndCheat.Pos.X+1, posAndCheat.Pos.Y),
-                    new(posAndCheat.Pos.X-1, posAndCheat.Pos.Y),
-                    new(posAndCheat.Pos.X, posAndCheat.Pos.Y+1),
-                    new(posAndCheat.Pos.X, posAndCheat.Pos.Y-1)
-                };
-        if (posAndCheat.CheatPos != null)
-            return lst
+        return new List<Position>() {
+                    new(pos.X+1, pos.Y),
+                    new(pos.X-1, pos.Y),
+                    new(pos.X, pos.Y+1),
+                    new(pos.X, pos.Y-1)
+                }
                 .Where(pos => pos.X >= 0 && pos.X < _map[0].Length && pos.Y >= 0 && pos.Y < _map.Length)
-                .Where(pos => _map[pos.X][pos.Y] != '#')
-                .Select(pos => new PosAndCheatPos(pos, posAndCheat.CheatPos));
-        else
-            return lst
-                .Where(pos => pos.X >= 0 && pos.X <= _map[0].Length && pos.Y >= 0 && pos.Y <= _map.Length)
-                .Select(pos =>
-                {
-                    var cheatPos = _map[pos.X][pos.Y] == '#' ? pos : null;
-                    return new PosAndCheatPos(pos, cheatPos);
-                });
+                .Where(pos => _map[pos.X][pos.Y] != '#');
     }
 
     private static string MapToString(char[][] map, Dictionary<Position, int>? exploredPositions = null)
@@ -133,5 +154,3 @@ public class Day20 : IDaySolution
             .ToArray();
     }
 }
-
-internal record PosAndCheatPos(Position Pos, Position? CheatPos);
