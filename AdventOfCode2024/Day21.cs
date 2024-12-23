f08d9bd6983e9349f4f714748327f5572d81268b
Anthony Dunford <Anthony@Dunford.uk.com>
2024-12-23 15:28:11 +0000

    Day 21 Part 1

--- AdventOfCode2024/Day21.cs
+++ AdventOfCode2024/Day21.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode2024;
@@ -11,18 +10,15 @@ public class Day21 : IDaySolution
 
         return codes.Select(code =>
         {
-            Console.WriteLine($"{code}");
-
-            string buttonSequence1 = GetButtonSequence(code, GetRobotInstructionsToPressKeypad);
-            Console.WriteLine($"{buttonSequence1}");
+            var buttonSequence1 = GetButtonSequence(code, GetRobotInstructionsToPressKeypad);
             
-            string buttonSequence2 = GetButtonSequence(buttonSequence1, GetRobotInstructionsToPressArrows);
-            Console.WriteLine($"{buttonSequence2}");
+            var buttonSequence2 = buttonSequence1.SelectMany(bs => GetButtonSequence(bs, GetRobotInstructionsToPressArrows));
 
-            string buttonSequence3 = GetButtonSequence(buttonSequence2, GetRobotInstructionsToPressArrows);
-            Console.WriteLine($"{buttonSequence3}");
+            var buttonSequence3 = buttonSequence2.SelectMany(bs => GetButtonSequence(bs, GetRobotInstructionsToPressArrows));
 
-            return GetCodeNum(code) * buttonSequence3.Length;
+            var minBs = buttonSequence3.MinBy(bs => bs.Length)!;
+
+            return GetCodeNum(code) * minBs.Length;
         }).Sum();
     }
 
@@ -38,18 +34,24 @@ public class Day21 : IDaySolution
         return int.Parse(match.ToString());
     }
 
-    private static string GetButtonSequence(string code, GetRobotInstruction GetRobotInstructionFunc)
+    private static List<string> GetButtonSequence(string code, GetRobotInstruction GetRobotInstructionFunc)
     {
         var currentKey = 'A';
-        return string.Concat(
-            code.ToCharArray()
-            .Select(nextKey =>
-            {
-                var movements = GetRobotInstructionFunc(currentKey, nextKey);
-                currentKey = nextKey;
-                return movements + 'A';
-            })
-        );
+
+        var possibleSequences = new List<string>{""};
+
+        foreach (var nextKey in code)
+        {
+            var movements = GetRobotInstructionFunc(currentKey, nextKey);
+            possibleSequences = possibleSequences
+                .SelectMany(s => movements.Select(m =>
+                    s + m + 'A'
+                ))
+                .ToList();
+            currentKey = nextKey;
+        }
+
+        return possibleSequences;
     }
 
     private static IEnumerable<string> GetRobotInstructionsToPressKeypad(char currentKey, char nextKey)
@@ -72,64 +74,64 @@ public class Day21 : IDaySolution
             ('1', '2') => [">"],
             ('1', '3') => [">>"],
             ('1', '4') => ["^"],
-            ('1', '5') => [">^", "^>"],
-            ('1', '6') => [">>^", "^>>"],
+            ('1', '5') => ["^>", ">^"],
+            ('1', '6') => ["^>>", ">>^"],
             ('1', '7') => ["^^"],
-            ('1', '8') => [">^^", "^^>"],
-            ('1', '9') => [">>^^", "^^>>"],
-            ('1', 'A') => [">>v", ">v>"],
+            ('1', '8') => ["^^>", ">^^"],
+            ('1', '9') => ["^^>>", ">>^^"],
+            ('1', 'A') => [">>v"],
             ('1', '0') => [">v"],
 
             ('2', '1') => ["<"],
             ('2', '2') => [""],
             ('2', '3') => [">"],
-            ('2', '4') => ["<^", "^<"],
+            ('2', '4') => ["^<", "<^"],
             ('2', '5') => ["^"],
-            ('2', '6') => [">^", "^>"],
-            ('2', '7') => ["<^^", "^^<"],
+            ('2', '6') => ["^>", ">^"],
+            ('2', '7') => ["^^<", "<^^"],
             ('2', '8') => ["^^"],
-            ('2', '9') => [">^^", "^^>"],
-            ('2', 'A') => [">v"],
+            ('2', '9') => ["^^>", ">^^"],
+            ('2', 'A') => ["v>", ">v"],
             ('2', '0') => ["v"],
 
             ('3', '1') => ["<<"],
             ('3', '2') => ["<"],
             ('3', '3') => [""],
-            ('3', '4') => ["^<<", "<^<"],
+            ('3', '4') => ["^<<", "<<^"],
             ('3', '5') => ["^<", "<^"],
             ('3', '6') => ["^"],
-            ('3', '7') => ["^^<<", "<<^^"],
+            ('3', '7') => ["<<^^", "^^<<"],
             ('3', '8') => ["^^<", "<^^"],
             ('3', '9') => ["^^"],
-            ('3', 'A') => [""],
-            ('3', '0') => ["v<", "<v"],
+            ('3', 'A') => ["v"],
+            ('3', '0') => ["<v", "v<"],
 
             ('4', '1') => ["v"],
-            ('4', '2') => ["v>"],
+            ('4', '2') => ["v>", ">v"],
             ('4', '3') => ["v>>", ">>v"],
             ('4', '4') => [""],
             ('4', '5') => [">"],
             ('4', '6') => [">>"],
             ('4', '7') => ["^"],
-            ('4', '8') => ["v>", ">^"],
+            ('4', '8') => ["^>", ">^"],
             ('4', '9') => ["^>>", ">>^"],
             ('4', 'A') => [">>vv"],
             ('4', '0') => [">vv"],
 
-            ('5', '1') => ["v<", "<v"],
-            ('5', '5') => ["v"],
+            ('5', '1') => ["<v", "v<"],
+            ('5', '2') => ["v"],
             ('5', '3') => ["v>", ">v"],
             ('5', '4') => ["<"],
-            ('5', '2') => ["v"],
+            ('5', '5') => [""],
             ('5', '6') => [">"],
-            ('5', '7') => ["<^", "^<"],
+            ('5', '7') => ["^<", "<^"],
             ('5', '8') => ["^"],
-            ('5', '9') => [">^", "^>"],
+            ('5', '9') => ["^>", ">^"],
             ('5', 'A') => ["vv>", ">vv"],
             ('5', '0') => ["vv"],
 
-            ('6', '1') => ["v<<", "<<v"],
-            ('6', '2') => ["v<", "<v"],
+            ('6', '1') => ["<<v", "v<<"],
+            ('6', '2') => ["<v", "v<"],
             ('6', '3') => ["v"],
             ('6', '4') => ["<<"],
             ('6', '5') => ["<"],
@@ -137,8 +139,8 @@ public class Day21 : IDaySolution
             ('6', '7') => ["^<<", "<<^"],
             ('6', '8') => ["^<", "<^"],
             ('6', '9') => ["^"],
-            ('6', 'A') => ["v"],
-            ('6', '0') => ["vv<", "<vv"],
+            ('6', 'A') => ["vv"],
+            ('6', '0') => ["<vv", "vv<"],
 
             ('7', '1') => ["vv"],
             ('7', '2') => ["vv>", ">vv"],
@@ -152,29 +154,29 @@ public class Day21 : IDaySolution
             ('7', 'A') => [">>vvv"],
             ('7', '0') => [">vvv"],
 
-            ('8', '1') => ["vv<", "vv<"],
-            ('8', '5') => ["vv"],
+            ('8', '1') => ["<vv", "vv<"],
+            ('8', '2') => ["vv"],
             ('8', '3') => ["vv>", ">vv"],
-            ('8', '4') => ["v<", "<v"],
-            ('8', '2') => ["v"],
+            ('8', '4') => ["<v", "v<"],
+            ('8', '5') => ["v"],
             ('8', '6') => ["v>", ">v"],
             ('8', '7') => ["<"],
             ('8', '8') => [""],
             ('8', '9') => [">"],
-            ('8', 'A') => [">vvv", "vvv>"],
+            ('8', 'A') => ["vvv>", ">vvv"],
             ('8', '0') => ["vvv"],
 
-            ('9', '1') => ["vv<<", "<<vv"],
-            ('9', '2') => ["vv<", "<vv"],
-            ('9', '3') => ["v"],
-            ('9', '4') => ["<<"],
-            ('9', '5') => ["v<", "<v"],
+            ('9', '1') => ["<<vv", "vv<<"],
+            ('9', '2') => ["<vv", "vv<"],
+            ('9', '3') => ["vv"],
+            ('9', '4') => ["<<v", "v<<"],
+            ('9', '5') => ["<v", "v<"],
             ('9', '6') => ["v"],
             ('9', '7') => ["<<"],
             ('9', '8') => ["<"],
             ('9', '9') => [""],
             ('9', 'A') => ["vvv"],
-            ('9', '0') => ["vvv<", "<vvv"],
+            ('9', '0') => ["<vvv", "vvv<"],
 
             ('A', '1') => ["^<<"],
             ('A', '2') => ["^<", "<^"],
@@ -191,16 +193,16 @@ public class Day21 : IDaySolution
         };
     }
 
-    private IEnumerable<string> GetRobotInstructionsToPressArrows(char currentKey, char nextKey)
+    private static IEnumerable<string> GetRobotInstructionsToPressArrows(char currentKey, char nextKey)
     {
         return (currentKey, nextKey) switch
         {
             ('A', '^') => ["<"],
             ('A', '<') => ["v<<"],
-            ('A', 'v') => ["v<", "v<"],
+            ('A', 'v') => ["<v", "v<"],
             ('A', '>') => ["v"],
             ('A', 'A') => [""],
-            
+
             ('^', '^') => [""],
             ('^', '<') => ["v<"],
             ('^', 'v') => ["v"],
@@ -212,23 +214,57 @@ public class Day21 : IDaySolution
             ('<', 'v') => [">"],
             ('<', '>') => [">>"],
             ('<', 'A') => [">>^"],
-            
+
             ('v', '^') => ["^"],
             ('v', '<') => ["<"],
             ('v', 'v') => [""],
             ('v', '>') => [">"],
-            ('v', 'A') => [">^", "^>"],
-            
+            ('v', 'A') => ["^>", ">^"],
+
             ('>', '^') => ["^<", "<^"],
             ('>', '<') => ["<<"],
             ('>', 'v') => ["<"],
             ('>', '>') => [""],
             ('>', 'A') => ["^"],
-            
+
             _ => ["?"]
         };
     }
 
+    private static char GetArrowFromInstructions(string instructions, char startArrow = 'A')
+    {
+        if (instructions == "")
+            return startArrow;
+        return (startArrow, instructions[0]) switch
+        {
+            ('A', '<') => GetArrowFromInstructions(instructions[1..], '^'),
+            ('A', 'v') => GetArrowFromInstructions(instructions[1..], '>'),
+
+            ('^', 'v') => GetArrowFromInstructions(instructions[1..], 'v'),
+            ('^', '>') => GetArrowFromInstructions(instructions[1..], 'A'),
+
+            ('<', '>') => GetArrowFromInstructions(instructions[1..], 'v'),
+
+            ('v', '^') => GetArrowFromInstructions(instructions[1..], '^'),
+            ('v', '<') => GetArrowFromInstructions(instructions[1..], '<'),
+            ('v', '>') => GetArrowFromInstructions(instructions[1..], '>'),
+
+            ('>', '^') => GetArrowFromInstructions(instructions[1..], 'A'),
+            ('>', '<') => GetArrowFromInstructions(instructions[1..], 'v'),
+
+            _ => '?'
+        };
+    }
+
+    private static string TraverseInstructions(string instructions)
+    {
+        var startingChar = 'A';
+        return string.Concat(instructions.Split('A').Select(c =>
+        {
+            startingChar = GetArrowFromInstructions(c, startingChar);
+            return $"{startingChar}";
+        }));
+    }
 
     internal static IEnumerable<string> ParseInput()
     {
@@ -236,4 +272,4 @@ public class Day21 : IDaySolution
     }
 }
 
-internal delegate string GetRobotInstruction(char currentKey, char nextKey);
+internal delegate IEnumerable<string> GetRobotInstruction(char currentKey, char nextKey);
